5aac1387e3a67337b47cd39c6be09f2e0f3167fb
minhtan0802 <minhtan0802@gmail.com>
2021-09-10 16:52:51 +0700

    Project

---
+++ QLVT/Form1.vb
@@ -0,0 +1,12 @@
+﻿Partial Public Class Form1
+
+    Public Sub New()
+        InitializeComponent()
+    End Sub
+    Private Sub navBarControl_ActiveGroupChanged(ByVal sender As Object, ByVal e As DevExpress.XtraNavBar.NavBarGroupEventArgs) Handles navBarControl.ActiveGroupChanged
+        navigationFrame.SelectedPageIndex = navBarControl.Groups.IndexOf(e.Group)
+    End Sub
+    Private Sub barButtonNavigation_ItemClick(ByVal sender As Object, ByVal e As DevExpress.XtraBars.ItemClickEventArgs) Handles employeesBarButtonItem.ItemClick, customersBarButtonItem.ItemClick
+        navBarControl.ActiveGroup = If(e.Item.Caption = "Employees", employeesNavBarGroup, customersNavBarGroup)
+    End Sub
+End Class
